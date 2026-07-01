1c629577255ac50540143286b4853cf73b20c3cb
suryaanandasundaram <114812349+suryaanandasundaram@users.noreply.github.com>
2026-07-01 18:42:11 +0530

    Day4 Assignment

---
+++ Day4Assignment/login_RedBus_Flipkart_MultipleContexts.spec.ts
@@ -0,0 +1,26 @@
+import {test,chromium,webkit } from "@playwright/test";
+
+test("Red Bus in Edge Browser Instances",async ()=>{
+    const browserInstance = await chromium.launch({headless:false,channel:"msedge"});
+    const browserContext = await browserInstance.newContext();
+    const page = await browserContext.newPage();
+
+    await page.goto("https://www.redbus.in/");
+    const url = await page.url();
+    console.log(`URL of RedBus Website is ${url}`);
+    const title = await page.title();
+    console.log(`Title of RedBus WebSite is  ${title}`);
+
+});
+test("Flipkart in Firefox Browser Instances",async()=>{
+   const browserInstance = await webkit.launch({headless:false});
+   const browserContext  = await browserInstance.newContext();
+   const page = await browserContext.newPage();
+
+   await page.goto("https://www.flipkart.com/");
+   const url = await page.url();
+   console.log(`URL of Filpkart Website is ${url}`);
+   const title = await page.title();
+   console.log(`Title of Filpkart Website is ${title}`);
+
+});
