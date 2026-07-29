4800cd682d349046c5cad50488f6110fff50b37f
suryaanandasundaram <114812349+suryaanandasundaram@users.noreply.github.com>
2026-07-29 11:37:41 +0530

    Day1-ClassRoomActivity

---
+++ Day1/01-datatypes.js
@@ -0,0 +1,42 @@
+var number = 909243792
+console.log(number);
+console.log(typeof number);
+
+
+var string = "Hello, World!";
+console.log(string);
+console.log(typeof string);
+
+var isActive = true;
+console.log(isActive);
+console.log(typeof isActive);
+
+var accountNumber;
+console.log(accountNumber);
+console.log(typeof accountNumber);
+
+var emptyValue = null;
+console.log(emptyValue);
+console.log(typeof emptyValue);
+
+//Let Keyword
+
+/*FirstName  b) companyName  c) mobileNumber  d) isAutomation  e) hasPlaywright (do not assign)  
+*/
+let firstName = "John";
+let companyName = "Tech Solutions";
+let mobileNumber = 1234567890;
+let isAutomation = true;
+let hasPlaywright;
+
+console.log(firstName);
+console.log(companyName);
+console.log(mobileNumber);
+console.log(isAutomation);
+console.log(hasPlaywright);
+
+console.log(typeof firstName);
+console.log(typeof companyName);
+console.log(typeof mobileNumber);
+console.log(typeof isAutomation);
+console.log(typeof hasPlaywright);
